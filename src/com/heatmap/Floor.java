7bc45426477cb222cbeb2b9d0e6ea7c54d9c017a
Daniel Neel <dneelyep@gmail.com>
2012-10-05 18:02:11 -0400

    Much more progress setting up the GUI.

--- src/com/heatmap/Floor.java
+++ src/com/heatmap/Floor.java
@@ -6,4 +6,27 @@ import java.util.ArrayList;
 public class Floor {
 	private ArrayList ranges;
 
+	/** Create a new Floor with numRanges ranges. */
+	public Floor(int numRanges) {
+		ranges = new ArrayList(numRanges);
+	}
 }
+
+// 3rd floor book shelves:
+// 15 * 2 = 30
+// 13 * 2 = 26
+// 7      =  7
+// 12 * 2 = 24
+// 11 * 2 = 22
+// 24     = 24
+// 15 * 2 = 30
+// ===========
+//         163
+
+
+// 4th floor book shelves:
+// 15 * 2 = 30
+// 11 * 3 = 33
+// 15 * 2 = 30
+// ===========
+//          93
--- src/com/heatmap/GUI.java
+++ src/com/heatmap/GUI.java
@@ -1,6 +1,10 @@
 package com.heatmap;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
@@ -8,24 +12,48 @@ import javax.swing.SwingUtilities;
  *
  * @author Daniel Neel */
 public class GUI extends JApplet {
-
-    public static void main(String[] args) {
-    	System.out.println("Test again.");
-    }
+	
+	/** Button to allow the user to view the 3rd floor's status. */
+	private JButton thirdFloor;
+	
+	/** Button to allow the user to view the 4th floor's status. */
+	private JButton fourthFloor;
 
     /** <Main method javadocs> */
     public void init() {
-	try {
+    	try {
             SwingUtilities.invokeAndWait(new Runnable() {
             	public void run() {
-            		JLabel lbl = new JLabel("YIPPEE! Finally!");
-            		add(lbl);
-            		JLabel testLabel = new JLabel("Testing.");
-            		add(testLabel);
+            		createGUI();
             	}
-		});
+            });
         } catch (Exception e) {
             System.err.println("createGUI didn't complete successfully");
         }
+    }
+    
+    /** Set up the GUI used for the program.*/
+    public void createGUI() {    	
+    	setLayout(new GridBagLayout());
+    	GridBagConstraints g = new GridBagConstraints();
+
+    	g.gridx = 2;
+    	g.gridy = 0;
+    	add(new JLabel("Stacks Cleanliness Heat Map"), g);
+    	
+    	g.gridx = 2;
+    	g.gridy = 1;
+    	add(new JLabel(new ImageIcon("../res/bin/fourthFloor.png")), g);
+    	
+    	thirdFloor = new JButton("3rd floor");
+    	fourthFloor = new JButton("4th floor");
+    	
+    	g.gridx = 0;
+    	g.gridy = 2;
+    	add(thirdFloor, g);
+    	
+    	g.gridx = 1;
+    	g.gridy = 2;
+    	add(fourthFloor, g);
     }
 }
