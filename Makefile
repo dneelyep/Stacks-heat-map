ef2e5f026caa0782e2a2c76d87412cddcac1454b
Daniel Neel <dneelyep@gmail.com>
2012-10-05 13:57:34 -0400

    Progress on setting up the program as an applet.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 JC = javac
 
 all: bin/com/heatmap/GUI.class
-	jar -cfm heatmap.jar bin/Manifest.txt bin/com/heatmap/*.class
+	jar -cfm bin/heatmap.jar bin/Manifest.txt bin/com/heatmap/*.class
 
 bin/com/heatmap/GUI.class: src/com/heatmap/GUI.java
 	$(JC) src/com/heatmap/GUI.java -d bin/
--- src/com/heatmap/GUI.java
+++ src/com/heatmap/GUI.java
@@ -1,6 +1,8 @@
 package com.heatmap;
 
 import javax.swing.JApplet;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 /** GUI.java - Class that holds the primary GUI for the program.
  *
@@ -8,11 +10,20 @@ import javax.swing.JApplet;
 public class GUI extends JApplet {
 
     public static void main(String[] args) {
-	System.out.println("Test again.");
+    	System.out.println("Test again.");
     }
 
     /** <Main method javadocs> */
     public void init() {
-	System.out.println("Testing...");
+	try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+		    public void run() {
+			JLabel lbl = new JLabel("Hello World");
+			add(lbl);
+		    }
+		});
+        } catch (Exception e) {
+            System.err.println("createGUI didn't complete successfully");
+        }
     }
 }
